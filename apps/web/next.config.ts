8d93ad570a8175e3ba182556ae9fb4dae31fd839
Santi <santiagodaza3@gmail.com>
2025-10-19 04:38:03 -0400

    Relaxed CI while we use the front-end mainly for testing

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+const isCI = process.env.CI === "true";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // Don't fail the build in CI on lint or TS issues (while we're just testing things)
+  eslint: { ignoreDuringBuilds: isCI },
+  typescript: { ignoreBuildErrors: isCI },
+
+  // Tell Turbopack THIS folder is the root to silence the lockfile warning
+  turbopack: { root: __dirname },
 };
 
 export default nextConfig;
